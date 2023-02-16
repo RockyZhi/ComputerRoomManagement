e4fd6427044d3406445e84901bc8d0e9190846b6
RockyZhi <1150968585@qq.com>
2023-02-16 20:41:05 +0800

    final

---
+++ head/globalFile.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//管理员文件
+#define ADMIN_FILE     "ComputerRoomManagement\\admin.txt"
+//学生文件
+#define STUDENT_FILE   "ComputerRoomManagement\\student.txt"
+//教师文件
+#define TEACHER_FILE   "ComputerRoomManagement\\teacher.txt"
+//机房信息文件
+#define COMPUTER_FILE  "ComputerRoomManagement\\computerRoom.txt"
+//订单文件
+#define ORDER_FILE     "ComputerRoomManagement\\order.txt"
